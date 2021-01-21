9189f29a14d7cc03545274f670ae58315e239d87
louisshwliu <louisshwliu@tencent.com>
2021-01-21 14:27:08 +0800

    提交复现代码

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import { DarukServer, controller, get } from "daruk";
+import { DarukServer, controller, get, inject } from "daruk";
+import { ServiceName } from "./typings/define";
 
 (async () => {
   const myapp = DarukServer();
 
   @controller()
   class Index {
+    // 以下注入后报错
+    // @inject(ServiceName.Test) private _test;
     @get("/")
     public async index(ctx: any) {
+      // this._test.log();
       ctx.body = "hello world";
     }
   }
---
+++ src/typings/define.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-shadow */
+export enum ServiceName {
+    Test = 'Service_Test'
+}
